81da377e58190bcbd1c75edfb332582e1f0fa614
Joao Gabriel <jgafarias1@gmail.com>
2025-05-01 11:44:19 -0300

    Update

--- lib/widgets/category_grid.dart
+++ lib/widgets/category_grid.dart
@@ -2,12 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
 
 class CategoryGridItem extends StatelessWidget {
-  const CategoryGridItem({super.key, required this.category,});
+  const CategoryGridItem({super.key, required this.category});
 
   final Category category;
 
   @override
   Widget build(BuildContext context) {
-    return 
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withAlpha(150),
+            category.color.withAlpha(200),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+          color: Theme.of(context).colorScheme.onPrimary,
+        ),
+      ),
+    );
   }
 }
