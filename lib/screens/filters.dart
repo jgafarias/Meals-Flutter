c995fe7f8b50fac6ce00ca37a264b55a8ad806f8
Joao Gabriel <jgafarias1@gmail.com>
2025-05-05 10:23:34 -0300

    Update filters.dart

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals/screens/tabs.dart';
+import 'package:meals/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
@@ -16,6 +18,16 @@ class _FiltersScreenState extends State<FiltersScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Your Filters')),
+      drawer: MainDrawer(
+        onSelectScreen: (identifer) {
+          Navigator.of(context).pop();
+          if (identifer == 'meals') {
+            Navigator.of(
+              context,
+            ).push(MaterialPageRoute(builder: (ctx) => TabsScreen()));
+          }
+        },
+      ),
       body: Column(
         children: [
           SwitchListTile(
