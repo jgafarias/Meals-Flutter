c6e33b7893b1a0468d0a2993fd9fa73874b58f42
Joao Gabriel <jgafarias1@gmail.com>
2025-05-10 11:33:11 -0300

    Update

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:meals/providers/meals_providers.dart';
+import 'package:meals/providers/meals_provider.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
