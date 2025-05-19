236dd075d5956386d9f111997116f439d19d1545
Joao Gabriel <jgafarias1@gmail.com>
2025-05-19 09:10:38 -0300

    Update categories.dart

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -24,7 +24,7 @@ class _CategoriesScreenState extends State<CategoriesScreen>
 
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(microseconds: 300),
+      duration: const Duration(milliseconds: 500),
       lowerBound: 0,
       upperBound: 1,
     );
@@ -76,9 +76,15 @@ class _CategoriesScreenState extends State<CategoriesScreen>
         ],
       ),
       builder:
-          (context, child) => Padding(
-            padding: EdgeInsets.only(
-              top: 100 - _animationController.value * 100,
+          (context, child) => SlideTransition(
+            position: Tween(
+              begin: const Offset(0, 0.3),
+              end: const Offset(0, 0),
+            ).animate(
+              CurvedAnimation(
+                parent: _animationController,
+                curve: Curves.easeInOut,
+              ),
             ),
             child: child,
           ),
